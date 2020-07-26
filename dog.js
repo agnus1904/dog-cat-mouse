42733228914bf95ac91a8624974cfef693208112
khoa <agnus1904@gmail.com>
2020-07-26 23:45:41 +0700

    say hi method for dog

--- dog.js
+++ dog.js
@@ -1,8 +1,14 @@
+var chalk = require("chalk");
+
 function Dog(){
     this.stomatch = [];
 }
 Dog.prototype.eat = function(cat){
     this.stomatch.push(cat);
+}
+
+Dog.prototype.sayHi = function(){
+    console.log(chalk.green("abcd"));
 }
 
 module.exports = Dog;
---
+++ index.js
@@ -0,0 +1,5 @@
+var Dog = require("./Dog");
+
+var tom = new Dog("tom");
+
+tom.sayHi();
